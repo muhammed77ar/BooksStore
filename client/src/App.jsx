ec596231d3c575c075962277857cabec3110bcf3
Muhammad <mohamedar4477@gmail.com>
2024-10-30 15:07:47 +0100

    make some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,7 @@
 function App() {
 
   return (
-    <div className=" w-full h-screen flex justify-center items-center ">
-      <div className=" text-center text-3xl font-bold text-green-700 border-2 border-black bg-amber-200 px-12 py-4 rounded-xl">Hello World</div>
-    </div>
+    <></>
   )
 }
 
